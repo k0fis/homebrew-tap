a51e13752dd87863e28f596daa6485fe50fc770c
Pavel Drimalka <pavel.drimalka@icloud.com>
2026-05-19 23:37:57 +0200

    Add kfs-tg formula

---
+++ Formula/kfs-tg.rb
@@ -0,0 +1,29 @@
+class KfsTg < Formula
+  desc "Minimalist TUI Telegram client"
+  homepage "https://github.com/k0fis/kfs-tg"
+  version "0.1.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/k0fis/kfs-tg/releases/download/v#{version}/kfs-tg-macos-arm64"
+      sha256 "PLACEHOLDER"
+    else
+      url "https://github.com/k0fis/kfs-tg/releases/download/v#{version}/kfs-tg-macos-amd64"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  on_linux do
+    url "https://github.com/k0fis/kfs-tg/releases/download/v#{version}/kfs-tg-linux-amd64"
+    sha256 "PLACEHOLDER"
+  end
+
+  def install
+    bin.install Dir["kfs-tg*"].first => "kfs-tg"
+  end
+
+  test do
+    assert_match "kfs-tg", shell_output("#{bin}/kfs-tg --version")
+  end
+end
